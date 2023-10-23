4c798591efdead6cad3aa29e67a9a676ff9d7b08
JakaHatlakLJ <146855430+JakaHatlakLJ@users.noreply.github.com>
2023-10-23 23:52:23 +0200

    Update mcc_pi.m

--- mcc_pi.m
+++ mcc_pi.m
@@ -1,35 +1,37 @@
 function [krog, kvadrat] = mcc_pi(stevilo_tock, print_results)
-    % Check if print_results argument is provided
+    % Preveri če je print argument podan
     if nargin < 2
         print_results = true;
     end
 
-    % Generiraj naključne točke znotraj enotskega kvadrata
+    % Generiraj naključnih točk znotraj enotskega kvadrata
     x = rand(1, stevilo_tock);
     y = rand(1, stevilo_tock);
 
-    % Poglej, če so točke v krogu
+    % Preveri, ali so točke znotraj kroga
     razdalja = sqrt(x.^2 + y.^2);
     znotraj_kroga = razdalja <= 1;
 
-    % X in Y koordinate točke v krogu
+    % Koordinate točk znotraj kroga
     krog = [x(znotraj_kroga); y(znotraj_kroga)];
 
-    % X in Y koordinate točk v kvadratu
+    % Koordinate točk znotraj kvadrata
     kvadrat = [x; y];
 
-    % Če je print_results=true, vrni rezultat
+    % Anonimna funkcija za izračun točk na loku krožnice
+    izracunaj_lok = @(krog) sum(abs(sqrt(krog(1,:).^2 + krog(2,:).^2 - 1)) < 2e-1);
+
+    % Klic anonimne funkcije za izračun točk na loku krožnice
+    tocke_na_loku = izracunaj_lok(krog);
+
+    % Printa rezultat če, je print_results == true; pomembno za calc_pi
     if print_results
         disp('Točke znotraj kroga:');
         disp(krog');
 
         disp('Točke znotraj kvadrata:');
         disp(kvadrat');
+        
+        disp(['Točke na loku krožnice: ' num2str(tocke_na_loku)]);
     end
-    % Anonimna funkcija za izračun točk na loku krožnice
-    izracunaj_lok = @(krog) size(find(znotraj_kroga), 2);
-
-    % Klic anonimne funkcije za izračun točk na loku krožnice
-    tocke_na_loku = izracunaj_lok(krog);
-    disp(['Točke na loku krožnice: ' num2str(tocke_na_loku)]);
 end
